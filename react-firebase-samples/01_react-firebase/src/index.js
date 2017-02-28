130d7f16d543725f4636891c7b00ff099eefe085
zeeshanhanif <zeeshanhanif@gmail.com>
2017-02-28 10:23:49 +0500

    React with core firebase sdk. Updating value on firebase

--- react-firebase-samples/01_react-firebase/src/index.js
+++ react-firebase-samples/01_react-firebase/src/index.js
@@ -8,8 +8,7 @@ import * as firebase from 'firebase'
 var firebaseConfig = {
     apiKey: "AIzaSyDq4YtXDQtQbY7EgySPTjBmq3QHMN8Fs8k",
     authDomain: "demotest-8c044.firebaseapp.com",
-    databaseURL: "https://demotest-8c044.firebaseio.com",
-    storageBucket: "demotest-8c044.appspot.com"
+    databaseURL: "https://demotest-8c044.firebaseio.com"
 };
 firebase.initializeApp(firebaseConfig);
 
